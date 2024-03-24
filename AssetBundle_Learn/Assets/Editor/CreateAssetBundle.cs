69dbe609869200aa728013ba5ee112c15e2ac607
Russell Xin <1612650023@qq.com>
2024-03-24 23:07:29 +0000

    first conmmit

---
+++ AssetBundle_Learn/Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using UnityEditor;
+
+public class CreateAssetBundle
+{
+    [MenuItem("AssetBundle/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+            BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows);
+    }
+}
